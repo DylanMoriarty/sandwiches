47622961a975852245ff52a0e61f0c86b57e40f9
DylanMoriarty <EatinCake@gmail.com>
2015-04-09 21:19:17 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /****** GLOBAL VARIABLES *******/
-var width = 960, height = 460;
+var width = 840, height = 460;
 
 /*---*******---END OF GLOBAL VARIABLES---*******---*/
 //--------------------------------------------------/
@@ -14,7 +14,7 @@ function setMap(){
 
 	var projection = d3.geo.albers()
 		.center([0, 43.0736])
-		.rotate([89.3988])
+		.rotate([89.3965])
 		.parallels([50, 60])
 	    .scale(1800000)
 	    .translate([width / 2, height / 2])
@@ -22,35 +22,23 @@ function setMap(){
 	var path = d3.geo.path()
 		.projection(projection);
 
-	var voronoi = d3.geom.voronoi()
-	    .x(function(d) { return d.x; })
-	    .y(function(d) { return d.y; })
-	    .clipExtent([[0, 0], [width, height]]);
-
+	// var voronoi = d3.geom.voronoi()
+	//     .x(function(d) { return d.x; })
+	//     .y(function(d) { return d.y; })
+	//     .clipExtent([[0, 0], [width, height]]);
 
 	var svg = d3.select("body").append("svg")
 		.attr("width", width)
 		.attr("height", height)
 
 	queue()
+		.defer(d3.json, "data/voronoi.json")
 		.defer(d3.json, "data/madison.json")
 		.defer(d3.csv, "data/sandwiches.csv")
 		.await(ready);
 
-	function ready(error, mad, shops){
+	function ready(error, voronoi, mad, shops){
 		var positions = [];
-
-		svg.append("path")
-			.datum(topojson.feature(mad, mad.objects.MadisonBuildings))
-			.attr("class", "buildings")
-			.attr("d", path);
-
-		svg.append("path")
-			.datum(topojson.feature(mad, mad.objects.MadisonWater))
-			.attr("class", "water")
-			.attr("d", path);		
-
-		console.log(shops)
 
         shops = shops.filter(function(d){
         	if (d.count = shops.length){
@@ -63,22 +51,45 @@ function setMap(){
         	}
         })
 
-    	voronoi(shops)
-       		 .forEach(function(d) { d.point.cell = d; });
+        for (var i=0; i<shops.length; i++){
+        // console.log(shops[i])
+        	};
+
+        console.log(voronoi)
+
+    	// voronoi(shops)
+     //   		 .forEach(function(d) { d.point.cell = d; });
+
+		svg.append("path")
+			.datum(topojson.feature(mad, mad.objects.MadisonBuildings))
+			.attr("class", "buildings")
+			.attr("d", path);
+
+		svg.append("path")
+			.datum(topojson.feature(mad, mad.objects.MadisonWater))
+			.attr("class", "water")
+			.attr("d", path);		
+
+     	svg.append("path")
+			.datum(topojson.feature(voronoi, voronoi.objects.voronoi4))
+			.attr("class", "lines")
+			.attr("d", path)
+			.on("mouseover", highlight)
+			.on("mouseout", dehighlight)			; 	
 
  		var shops = svg.append("g")
        	 .attr("class", "shops")
        	 .selectAll("g")
-         .data(shops.sort(function(a, b) { return b.count - a.count; }))
+         .data(shops)
          .enter()
          .append("g")
          .attr("class", "shops")
 
- 	   shops.append("path")
-	        .attr("class", "cell")
-	        .attr("d", function(d) { return d.cell.length ? "M" + d.cell.join("L") + "Z" : null; });
 
-        console.log(shops)
+
+ 	   // shops.append("path")
+	    //     .attr("class", "cell")
+	    //     .attr("d", function(d) { return d.cell.length ? "M" + d.cell.join("L") + "Z" : null; });
 
         shops.append("circle")
         	.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
